fb9bf0c0611bd5da3ed00a4daff2805a99990b91
Louis LeFebvre <louislefebvre1999@gmail.com>
2026-02-28 12:58:38 -0600

    feat: Read in args from cmdline & print

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,21 @@
+// https://ziglang.org/documentation/0.15.2/std
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const allocator = gpa.allocator();
+
+    var buf: [1024]u8 = undefined;
+    var stdW = std.fs.File.stdout().writer(&buf);
+    const stdout = &stdW.interface;
+
+    var args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    for (args[1..]) |arg| {
+        try stdout.print("{s} ", .{arg});
+    }
+    try stdout.print("\n", .{});
+    try stdout.flush();
 }
